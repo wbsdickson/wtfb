03b14bd920b5f81c8e9dec157910a7667c87583c
yky32 <yky32is@gmail.com>
2018-06-30 02:48:06 +0800

    init
    setup

---
+++ src/main/java/com/ykyclm/wtfb/WtfbApplication.java
@@ -0,0 +1,12 @@
+package com.ykyclm.wtfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtfbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtfbApplication.class, args);
+	}
+}
